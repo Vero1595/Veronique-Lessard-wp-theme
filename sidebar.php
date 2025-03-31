d17c4f86ae902757f1d9333d98fd114344a4da8a
Vero1595 <1595883@johnabbottcollege.net>
2025-03-31 11:49:40 -0400

    Pain

---
+++ sidebar.php
@@ -0,0 +1,18 @@
+    <!-- START STEP 2.22 ADD SIDEBAR -->
+
+    <?php get_sidebar(); ?>
+
+    <!-- END STEP 2.22 ADD SIDEBAR -->
+
+    <?php // START BUILDING SIDEBAR
+        // Add this to all your php files for added security
+
+        if (!defined('ABSPATH'))
+            exit;
+
+        if ( is_active_sidebar('widget_1')  ) : ?>
+                <aside class="sidebar widget-area" role="complementary">
+                <?php dynamic_sidebar( 'widget_1' ); ?>
+        </aside>
+        <?php endif; 
+    // END BUILDING SIDEBAR ?>
